9f00682bdeef73c5cf5640ffb97e5975773c3b6f
udaygowda-cell <udaykjgowda@gmail.com>
2026-02-05 21:26:36 +0530

    new change

--- Main.java
+++ Main.java
@@ -130,7 +130,8 @@ public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.print("enter the numbers: ");
-        int n = sc.nextInt();
+        int n = sc.nextInt();//;
+        
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++){
                 System.out.print(j + " ");
