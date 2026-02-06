dcb79bfc715857ac4bbefd4b14bf821277522eae
udaygowda-cell <udaykjgowda@gmail.com>
2026-02-06 08:47:55 +0530

    Daily 06 update

--- Main.java
+++ Main.java
@@ -50,20 +50,20 @@ public class Main {
 }
 
 */
-/* 
-public class Main{
-    public static void main (String[] args){
-        int i;
-        int j;
-        for(i=1;i<9;i++){
-            for(j=1;j<i;j++){
-        System.out.print("*");
-            }
-        System.out.println();
-        }
-    }
-}
 
+// public class Main{
+//     public static void main (String[] args){
+//         int i;
+//         int j;
+//         for(i=1;i<9;i++){
+//             for(j=1;j<i;j++){
+//         System.out.print("*");
+//             }
+//         System.out.println();
+//         }
+//     }
+// }
+/* 
 
 import java.util.Scanner;
 
@@ -124,7 +124,7 @@ public class Main{
 
     }
 }
-    */
+  
 import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
@@ -139,4 +139,21 @@ public class Main{
             System.out.println();
         }
 }
+}
+*/
+
+public class Main
+{
+    public static void main(String  [] args)
+    {
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=1;j<=5;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
 }
