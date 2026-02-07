3af29757d6b51b682275f07b0d02571ae6914207
udaygowda-cell <udaykjgowda@gmail.com>
2026-02-07 11:01:55 +0530

    hall0

--- Main.java
+++ Main.java
@@ -171,8 +171,29 @@ public class Main
 //     }
 // }
 //multidimentional array
+// public class Main{
+//    public static void main(String[] args){
+//     int [] [] a= {{1,2,3}, {1,4,5}};
+//     System.out.println(a[0][2]);
+//    }
+// }
+
+//hallo
+
 public class Main{
-   public static void main(String[] args){
-    int [] [] a= {{1,2,3}, {1,4,5}};
-    System.out.println(a[0][2]);
-   }}
+    public static void main(String[] args){
+        for(int i=1;i<=5;i++){
+            for(int j =1;j<=5;j++){
+                if(i==1 || j== 5 || j==1 || i==5){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+            }
+               
+            }
+             System.out.println();
+        }
+    }
+
+}
