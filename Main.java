9217ca1c9446152ea43c57465e9456402b2e5b7d
udaygowda-cell <udaykjgowda@gmail.com>
2026-02-08 20:44:25 +0530

    Daily streak update

--- Main.java
+++ Main.java
@@ -180,6 +180,26 @@ public class Main
 // }
 
 //hallo
+// import java.util.Scanner;
+// public class Main{
+//     public static void main(String[] args){
+//     Scanner sc = new Scanner(System.in);
+// System.out.println("enter the size: ");
+// int n = sc.nextInt();
+//         for(int i=1;i<=n;i++){
+//             for(int j =1;j<=n;j++){
+//                 if(i==1 || j== 5 || j==1 || i==5){
+//                     System.out.print("* ");
+//                 }
+//                 else{
+//                     System.out.print("  ");
+//             }              
+//             }
+//              System.out.println();
+//         }
+//     }
+// } 
+//
 import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
@@ -187,16 +207,15 @@ public class Main{
 System.out.println("enter the size: ");
 int n = sc.nextInt();
         for(int i=1;i<=n;i++){
-            for(int j =1;j<=n;j++){
-                if(i==1 || j== 5 || j==1 || i==5){
-                    System.out.print("* ");
+            for(int s =1;s<=n-i;s++){
+                
+                    System.out.print(" ");
                 }
-                else{
-                    System.out.print("  ");
+                for (int j =1; j<=(2*i-1);j++){
+                    System.out.print("*");
             }              
-            }
+            
              System.out.println();
         }
     }
-} 
-//// nefytdvus
+}
